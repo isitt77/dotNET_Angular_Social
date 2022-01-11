5059ed5ae13a6d6ae40c8bae501b19a32b486d76
Scott Isitt <isitts@hotmail.com>
2022-01-11 01:20:30 -0800

    Change CORS policy in Startup.cs to have https instead of http.

--- API/Startup.cs
+++ API/Startup.cs
@@ -55,7 +55,7 @@ namespace API
 
             app.UseCors(policy => policy.AllowAnyHeader()
             .AllowAnyMethod()
-            .WithOrigins("http://localhost:4200"));
+            .WithOrigins("https://localhost:4200"));
 
             app.UseAuthorization();
 
