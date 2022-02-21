47b8055fdf9d52def2cd4c9724831fcaeb1ec0cd
Scott Isitt <isitts@hotmail.com>
2022-02-21 15:24:45 -0800

    Change context to _context in UsersController.cs.

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -11,23 +11,23 @@ namespace API.Controllers
 {
     public class UsersController : BaseApiController
     {
-        private readonly DataContext context;
+        private readonly DataContext _context;
 
         public UsersController(DataContext context)
         {
-            this.context = context;
+            _context = context;
         }
         // Gets the collection of users
         [HttpGet]
         public async Task<ActionResult<IEnumerable<AppUser>>> GetUsers()
         {
-            return await context.Users.ToListAsync();
+            return await _context.Users.ToListAsync();
         }
         // Gets a single user by id
         [HttpGet("{id}")]
         public async Task<ActionResult<AppUser>> GetUser(int id)
         {
-            return await context.Users.FindAsync(id);
+            return await _context.Users.FindAsync(id);
         }
     }
 }
