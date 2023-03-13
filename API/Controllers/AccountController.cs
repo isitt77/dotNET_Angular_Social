f2b64bc3b72b6fae3151e97dc503117c0f85d7df
Scott Isitt <isitts@hotmail.com>
2023-03-13 00:05:16 -0700

    Add Login endpoint that checks username and password.

--- API/Controllers/AccountController.cs
+++ API/Controllers/AccountController.cs
@@ -28,8 +28,8 @@ namespace API.Controllers
                 return BadRequest("Username already exists.");
             }
 
-            using var hmac = new HMACSHA512();
-            AppUser user = new AppUser
+            using HMACSHA512 hmac = new();
+            AppUser user = new()
             {
                 UserName = registerDto.Username.ToLower(),
                 PasswordHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(
@@ -39,6 +39,34 @@ namespace API.Controllers
             _context.Users.Add(user);
             await _context.SaveChangesAsync();
 
+            return user;
+        }
+
+        [HttpPost("login")]
+        public async Task<ActionResult<AppUser>> Login(LoginDto loginDto)
+        {
+            AppUser user = await _context.Users.SingleOrDefaultAsync(
+                u => u.UserName == loginDto.Username);
+
+            // Check if user exists...
+            if (user == null) return Unauthorized(
+                "Invalid username or password.");
+
+            // Pass in user's salted password and check if entered
+            // password matches the database...
+            using HMACSHA512 hmac = new(user.PasswordSalt);
+
+            byte[] computedHash = hmac.ComputeHash(
+                Encoding.UTF8.GetBytes(loginDto.Password));
+
+            for (int i = 0; i < computedHash.Length; i++)
+            {
+                if (computedHash[i] != user.PasswordHash[i])
+                {
+                    return Unauthorized("Invalid username or password.");
+                }
+            }
+            // If conditions are met, return user...
             return user;
         }
 
