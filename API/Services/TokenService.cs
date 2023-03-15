06c8811ad4c1ee16ace0706d14a474fcdb3b26dc
Scott Isitt <isitts@hotmail.com>
2023-03-15 01:19:08 -0700

    Add claims logic to CreateToken().

--- API/Services/TokenService.cs
+++ API/Services/TokenService.cs
@@ -1,18 +1,48 @@
 ﻿using System;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
 using API.Entities;
 using API.Interfaces;
+using Microsoft.IdentityModel.Tokens;
 
 namespace API.Services
 {
     public class TokenService : ITokenService
     {
-        public TokenService()
+        private readonly SymmetricSecurityKey _key;
+
+        public TokenService(IConfiguration configuration)
         {
+            _key = new SymmetricSecurityKey(Encoding.UTF8
+                .GetBytes(configuration["TokenKey"]));
         }
 
         public string CreateToken(AppUser user)
         {
-            throw new NotImplementedException();
+            List<Claim> claims = new()
+            {
+                new Claim(JwtRegisteredClaimNames.NameId, user.UserName)
+            };
+
+            SigningCredentials signingCreds = new(
+                _key, SecurityAlgorithms.HmacSha512Signature
+                );
+
+            // Describes what's in our token...
+            SecurityTokenDescriptor tokenDescriptor = new()
+            {
+                Subject = new ClaimsIdentity(claims),
+                Expires = DateTime.UtcNow.AddDays(7),
+                SigningCredentials = signingCreds
+            };
+
+            JwtSecurityTokenHandler tokenHandler = new();
+
+            SecurityToken token = tokenHandler
+                .CreateToken(tokenDescriptor);
+
+            return tokenHandler.WriteToken(token);
         }
     }
 }
