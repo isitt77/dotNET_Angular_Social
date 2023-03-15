0fa0982b1153cfc67d42f8149b77153a3b61d81b
Scott Isitt <isitts@hotmail.com>
2023-03-14 19:38:18 -0700

    Create TokenService interface and class.

---
+++ API/Services/TokenService.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using API.Entities;
+using API.Interfaces;
+
+namespace API.Services
+{
+    public class TokenService : ITokenService
+    {
+        public TokenService()
+        {
+        }
+
+        public string CreateToken(AppUser user)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
+
--- API/Startup.cs
+++ API/Startup.cs
@@ -1,17 +1,7 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using API.Data;
-using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
+using API.Interfaces;
+using API.Services;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
 
 namespace API
@@ -34,6 +24,7 @@ namespace API
                     "DefaultConnection"));
             });
             services.AddControllers();
+            services.AddScoped<ITokenService, TokenService>();
             services.AddCors();
             services.AddSwaggerGen(c =>
             {
